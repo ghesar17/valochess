9d09d6ee2bab639b5f19befef3a7703c9b2009f0
nycshrike <112590067+ghesar17@users.noreply.github.com>
2023-01-23 01:32:55 -0500

    day 1 of building chess, got board to load

---
+++ load.py
@@ -0,0 +1,17 @@
+import os
+import pygame
+
+b_bishop = pygame.image.load(os.path.join('chess pngs', 'black bishop.png'))
+b_knight = pygame.image.load(os.path.join('chess pngs', 'black knight.png'))
+b_pawn = pygame.image.load(os.path.join('chess pngs', 'black pawn.png'))
+b_king = pygame.image.load(os.path.join('chess pngs', 'black king.png'))
+b_rook = pygame.image.load(os.path.join('chess pngs', 'black rook.png'))
+b_queen = pygame.image.load(os.path.join('chess pngs', 'black queen.png'))
+
+
+w_pawn = pygame.image.load(os.path.join('chess pngs', 'white pawn.png'))
+w_knight = pygame.image.load(os.path.join('chess pngs', 'white knight.png'))
+w_queen = pygame.image.load(os.path.join('chess pngs', 'white queen.png'))
+w_king = pygame.image.load(os.path.join('chess pngs', 'white king.png'))
+w_bishop = pygame.image.load(os.path.join('chess pngs', 'white bishop.png'))
+w_rook = pygame.image.load(os.path.join('chess pngs', 'white rook.png'))
---
+++ pieces.py
@@ -0,0 +1,11 @@
+import board
+
+
+class Piece:
+
+    def __init__(self, color):
+        self.color = color
+        self.turn = 'w'
+
+    # def bishop(self):
+    #     if turn
