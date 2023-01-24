4901404a2a2e13734d1a8c9d0d1b92a9963a29ac
nycshrike <112590067+ghesar17@users.noreply.github.com>
2023-01-24 00:17:16 -0500

    added board and started movement func

--- pieces.py
+++ pieces.py
@@ -1,67 +1,41 @@
-# from game import *
-# import pygame
-# import os
-
-# # b_knight = pygame.image.load(os.path.join('chess pngs', 'black knight.png'))
-# # b_pawn = pygame.image.load(os.path.join('chess pngs', 'black pawn.png'))
-# # b_king = pygame.image.load(os.path.join('chess pngs', 'black king.png'))
-# # b_rook = pygame.image.load(os.path.join('chess pngs', 'black rook.png'))
-# # b_queen = pygame.image.load(os.path.join('chess pngs', 'black queen.png'))
-# # b_bishop = pygame.image.load(os.path.join('chess pngs', 'black bishop.png'))
-
-# # w_pawn = pygame.image.load(os.path.join('chess pngs', 'white pawn.png'))
-# # w_knight = pygame.image.load(os.path.join('chess pngs', 'white knight.png'))
-# # w_queen = pygame.image.load(os.path.join('chess pngs', 'white queen.png'))
-# # w_king = pygame.image.load(os.path.join('chess pngs', 'white king.png'))
-# # w_bishop = pygame.image.load(os.path.join('chess pngs', 'white bishop.png'))
-# # w_rook = pygame.image.load(os.path.join('chess pngs', 'white rook.png'))
-
-# # b = [b_rook, b_knight, b_bishop, b_queen, b_king, b_pawn]
-# # w = [w_rook, w_knight, w_bishop, w_queen, w_king, w_pawn]
+import chess
 
 
-# class Piece:
-#     def __init__(self, row, col):
-#         self.row = row
-#         self.col = col
+class Piece:
+    def __init__(self, color):
+        self.color = color
 
-#     def draw_piece(self):
-#         if self.piece.isupper():
-#             # draw_this =
-#             pass
-
-#     def move(self):
-
-#         # p.Surface.blit(self.image(dest=(0, 0)))
+    def moves(self):
+        if turn % 2 == 0:
 
 
-# class Pawn(Piece):
+class Pawn(Piece):
 
 
-# class King(Piece):
+class King(Piece):
 
 
-# class Bishop(Piece):
+class Bishop(Piece):
 
 
-# class Knight(Piece):
+class Knight(Piece):
 
 
-# class Rook(Piece):
+class Rook(Piece):
 
 
-# class Queen(Piece):
+class Queen(Piece):
 
-#     # b_bishop = Bishop('b', 'w', b_bishop)
-#     # b_pawn = Pawn('b', 'w', b_pawn)
-#     # b_king = ing('b', 'w', b_king)
-#     # b_queen = Queen('b', 'w', b_queen)
-#     # b_rook = Rook('b', 'w', b_rook)
-#     # b_knight = Knight('b', 'w', b_knight)
+    # b_bishop = Bishop('b', 'w', b_bishop)
+    #     b_pawn = Pawn('b', 'w', b_pawn)
+    #     b_king = ing('b', 'w', b_king)
+    #     b_queen = Queen('b', 'w', b_queen)
+    #     b_rook = Rook('b', 'w', b_rook)
+    #     b_knight = Knight('b', 'w', b_knight)
 
-#     # w_bishop = Bishop('w', 'w', w_bishop)
-#     # w_pawn = Pawn('w', 'w', w_pawn)
-#     # w_king = King('w', 'w', w_king)
-#     # w_queen = Queen('w', 'w', w_queen)
-#     # w_rook = Rook('w', 'w', w_rook)
-#     # w_knight = Knight('w', 'w', w_knight)
+    #     w_bishop = Bishop('w', 'w', w_bishop)
+    #     w_pawn = Pawn('w', 'w', w_pawn)
+    #     w_king = King('w', 'w', w_king)
+    #     w_queen = Queen('w', 'w', w_queen)
+    #     w_rook = Rook('w', 'w', w_rook)
+    #     w_knight = Knight('w', 'w', w_knight)
