a84d1775e663726240d1a656f76a6ae627aaa9b1
Ghesar <ghesar4phanday@gmail.com>
2023-02-03 17:01:15 -0500

    added pieces

--- game.py
+++ game.py
@@ -2,12 +2,16 @@ from pieces import *
 import pygame as p
 import chess
 import os
-import load
 
 
 WIDTH, HEIGHT = 800, 800
+
+# SQ_WIDTH =
+
+
 screen = p.display.set_mode((WIDTH, HEIGHT))
 p.display.set_caption('VALOCHESS')
+p.display.flip()
 # p.display.set_icon(os.path.join('chess pngs', 'valornt.jpg'))
 
 
@@ -21,13 +25,18 @@ def main():
             if event.type == p.QUIT:
                 running = False
         draw_board()
+        draw_piece()
+        p.display.update()
     p.quit()
 
 
 def draw_board():
-    screen.blit(load.board_img, (0, 0))
-    b_bishop.draw_piece()
-    p.display.update()
+    screen.blit(p.image.load(os.path.join("chess pngs", 'board.png')), (0, 0))
+
+
+def draw_piece():
+    a_w.dra
+    screen.blit(p.image.load(os.path.join("chess pngs", 'wp.png')), (45, 590))
 
 
 if __name__ == '__main__':
