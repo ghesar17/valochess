61deff4c1c37a2399aa7fc6f5857da32e6d02d51
Ghesar <ghesar4phanday@gmail.com>
2023-02-04 14:37:27 -0500

    got pawn to load

--- game.py
+++ game.py
@@ -1,8 +1,94 @@
-from pieces import *
+from pieces import a_wp
 import pygame as p
-import chess
 import os
 
+
+class Piece:
+
+    def __init__(self, screen, turn, img, row, col, x, y):
+        self.turn = turn
+        self.screen = screen
+        self.img = img
+        self.row = row
+        self.col = col
+        self.x = x
+        self.y = y
+
+    def draw_piece(self):
+
+        self.screen.blit(p.image.load(os.path.join(
+            "chess pngs", self.img)), (self.x, self.y))
+
+    def move(self):
+        if self.turn % 2 == 0:
+            pass
+
+        else:
+            pass
+
+    def update_current_pos(self):
+        pass
+
+    def update_board(self):
+        pass
+
+
+class Pawn(Piece):
+
+    def __init__(self, screen, turn, img, row, col, x, y):
+        super().__init__(turn, screen, img, row, col, x, y)
+
+
+class King(Piece):
+
+    def __init__(self, turn, img, row, col, x, y):
+        super().__init__(turn, img, row, col, x, y)
+
+
+class Bishop(Piece):
+
+    def __init__(self, turn, img, row, col, x, y):
+        super().__init__(turn, img, row, col, x, y)
+
+
+class Knight(Piece):
+
+    def __init__(self, turn, img, row, col, x, y):
+        super().__init__(turn, img, row, col, x, y)
+
+
+class Rook(Piece):
+
+    def __init__(self, turn, img, row, col, x, y):
+        super().__init__(turn, img, row, col, x, y)
+
+
+class Queen(Piece):
+
+    def __init__(self, turn, img, row, col, x, y):
+        super().__init__(turn, img, row, col, x, y)
+
+
+# b_wp = Pawn(0, 'wp.png', '2', '2')
+# c_wp = Pawn(0, 'wp.png', '2', '3')
+# d_wp = Pawn(0, 'wp.png', '2', '4')
+# e_wp = Pawn(0, 'wp.png', '2', '5')
+# f_wp = Pawn(0, 'wp.png', '2', '6')
+# g_wp = Pawn(0, 'wp.png', '2', '7')
+# h_wp = Pawn(0, 'wp.png', '2', '8')
+
+
+# a_bp = Pawn(0, 'bp.png', '7', '1')
+# b_bp = Pawn(0, 'bp.png', '7', '2')
+# c_bp = Pawn(0, 'bp.png', '7', '3')
+# d_bp = Pawn(0, 'bp.png', '7', '4')
+# e_bp = Pawn(0, 'bp.png', '7', '5')
+# f_bp = Pawn(0, 'bp.png', '7', '6')
+# g_bp = Pawn(0, 'bp.png', '7', '7')
+# h_bp = Pawn(0, 'bp.png', '7', '8')
+
+
+# turn, img, row, col, x, y
 
 WIDTH, HEIGHT = 800, 800
 
@@ -12,10 +98,6 @@ WIDTH, HEIGHT = 800, 800
 screen = p.display.set_mode((WIDTH, HEIGHT))
 p.display.set_caption('VALOCHESS')
 p.display.flip()
-# p.display.set_icon(os.path.join('chess pngs', 'valornt.jpg'))
-
-
-# board = p.transform.scale(board_img, (800, 800))
 
 
 def main():
@@ -25,7 +107,8 @@ def main():
             if event.type == p.QUIT:
                 running = False
         draw_board()
-        draw_piece()
+        a_wp.draw_piece()
+        # pieces.a_wp.draw_piece()
         p.display.update()
     p.quit()
 
@@ -34,10 +117,12 @@ def draw_board():
     screen.blit(p.image.load(os.path.join("chess pngs", 'board.png')), (0, 0))
 
 
-def draw_piece():
-    a_w.dra
-    screen.blit(p.image.load(os.path.join("chess pngs", 'wp.png')), (45, 590))
+# def draw_pieces():
+#     pieces.a_wp.draw_piece()
+    # screen.blit(p.image.load(os.path.join("chess pngs", 'wp.png')), (45, 590))
 
+
+a_wp = Pawn(0, screen, 'wp.png', 2, 1, 45, 490)
 
 if __name__ == '__main__':
     main()
--- pieces.py
+++ pieces.py
@@ -1,41 +1,92 @@
-import chess
+# # import chess
+# import pygame as p
+# import game
+# import os
 
 
-class Piece:
-    def __init__(self, color):
-        self.color = color
+# class Piece:
 
-    def moves(self):
-        if turn % 2 == 0:
+#     def __init__(self, screen, turn, img, row, col, x, y):
+#         self.turn = turn
+#         self.screen = screen
+#         self.img = img
+#         self.row = row
+#         self.col = col
+#         self.x = x
+#         self.y = y
+
+#     def draw_piece(self):
+#         self.screen.blit(p.image.load(os.path.join(
+#             "chess pngs", self.img)), (self.x, self.y))
+
+#     def move(self):
+#         if self.turn % 2 == 0:
+#             pass
+
+#         else:
+#             pass
+
+#     def update_current_pos(self):
+#         pass
+
+#     def update_board(self):
+#         pass
 
 
-class Pawn(Piece):
+# class Pawn(Piece):
+
+#     def __init__(self, screen, turn, img, row, col, x, y):
+#         super().__init__(turn, screen, img, row, col, x, y)
 
 
-class King(Piece):
+# class King(Piece):
+
+#     def __init__(self, turn, img, row, col, x, y):
+#         super().__init__(turn, img, row, col, x, y)
 
 
-class Bishop(Piece):
+# class Bishop(Piece):
+
+#     def __init__(self, turn, img, row, col, x, y):
+#         super().__init__(turn, img, row, col, x, y)
 
 
-class Knight(Piece):
+# class Knight(Piece):
+
+#     def __init__(self, turn, img, row, col, x, y):
+#         super().__init__(turn, img, row, col, x, y)
 
 
-class Rook(Piece):
+# class Rook(Piece):
+
+#     def __init__(self, turn, img, row, col, x, y):
+#         super().__init__(turn, img, row, col, x, y)
 
 
-class Queen(Piece):
+# class Queen(Piece):
 
-    # b_bishop = Bishop('b', 'w', b_bishop)
-    #     b_pawn = Pawn('b', 'w', b_pawn)
-    #     b_king = ing('b', 'w', b_king)
-    #     b_queen = Queen('b', 'w', b_queen)
-    #     b_rook = Rook('b', 'w', b_rook)
-    #     b_knight = Knight('b', 'w', b_knight)
+#     def __init__(self, turn, img, row, col, x, y):
+#         super().__init__(turn, img, row, col, x, y)
 
-    #     w_bishop = Bishop('w', 'w', w_bishop)
-    #     w_pawn = Pawn('w', 'w', w_pawn)
-    #     w_king = King('w', 'w', w_king)
-    #     w_queen = Queen('w', 'w', w_queen)
-    #     w_rook = Rook('w', 'w', w_rook)
-    #     w_knight = Knight('w', 'w', w_knight)
+
+# a_wp = Pawn(0, game.screen, 'wp.png', 2, 1, 45, 490)
+# # b_wp = Pawn(0, 'wp.png', '2', '2')
+# # c_wp = Pawn(0, 'wp.png', '2', '3')
+# # d_wp = Pawn(0, 'wp.png', '2', '4')
+# # e_wp = Pawn(0, 'wp.png', '2', '5')
+# # f_wp = Pawn(0, 'wp.png', '2', '6')
+# # g_wp = Pawn(0, 'wp.png', '2', '7')
+# # h_wp = Pawn(0, 'wp.png', '2', '8')
+
+
+# # a_bp = Pawn(0, 'bp.png', '7', '1')
+# # b_bp = Pawn(0, 'bp.png', '7', '2')
+# # c_bp = Pawn(0, 'bp.png', '7', '3')
+# # d_bp = Pawn(0, 'bp.png', '7', '4')
+# # e_bp = Pawn(0, 'bp.png', '7', '5')
+# # f_bp = Pawn(0, 'bp.png', '7', '6')
+# # g_bp = Pawn(0, 'bp.png', '7', '7')
+# # h_bp = Pawn(0, 'bp.png', '7', '8')
+
+
+# # turn, img, row, col, x, y
