de256f4373eb631a3751d004bab84595a0c1d996
nycshrike <112590067+ghesar17@users.noreply.github.com>
2023-02-08 14:56:49 +0000

    made surface for a pawn

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import pieces
+import board
 import pygame as p
 import os
 
 WIDTH, HEIGHT = 800, 800
+transparent = (0, 0, 0, 0)
 
 SQ_WIDTH = 95
 
 screen = p.display.set_mode((WIDTH, HEIGHT))
 p.display.set_caption('VALOCHESS')
 p.display.flip()
+clock = p.time.Clock()
 
 
 def main():
@@ -17,57 +20,61 @@ def main():
         for event in p.event.get():
             if event.type == p.QUIT:
                 running = False
-            # elif event.type == p.MOUSEBUTTONDOWN:
-            #     mouse_pos = p.mouse.get_pos()
-            #     x = mouse_pos[0]
-            #     y = mouse_pos[1]
-                
-        draw_board()
-        draw_pieces()
+            elif event.type == p.MOUSEBUTTONDOWN:
+                mouse_pos = p.mouse.get_pos()
+                x = mouse_pos[0]
+                y = mouse_pos[1]
+                cursor_rect = p.Rect(x, y, 5, 5)
+                if p.Rect.colliderect(cursor_rect, pieces.a_wp.rect):
+                    pieces.a_wp.remove(pieces.a_wp.rect)
+        # board.current_board.draw_board()
+        clock.tick(1)
+        pieces.all_sprites.update()
+        pieces.all_sprites.draw(screen)
+        # draw_pieces()
+
         p.display.update()
     p.quit()
 
 
-def draw_board():
-    screen.blit(p.image.load(os.path.join("chess pngs", 'board.png')), (0, 0))
-
-
 def draw_pieces():
     pieces.a_wp.draw_piece()
-    pieces.b_wp.draw_piece()
-    pieces.c_wp.draw_piece()
-    pieces.d_wp.draw_piece()
-    pieces.e_wp.draw_piece()
-    pieces.f_wp.draw_piece()
-    pieces.g_wp.draw_piece()
-    pieces.h_wp.draw_piece()
+    # pieces.a_wp.draw_piece()
 
-    pieces.a_wr.draw_piece()
-    pieces.b_wn.draw_piece()
-    pieces.c_wb.draw_piece()
-    pieces.wq.draw_piece()
-    pieces.wk.draw_piece()
-    pieces.f_wb.draw_piece()
-    pieces.g_wn.draw_piece()
-    pieces.h_wr.draw_piece()
+    # pieces.b_wp.draw_piece()
+    # pieces.c_wp.draw_piece()
+    # pieces.d_wp.draw_piece()
+    # pieces.e_wp.draw_piece()
+    # pieces.f_wp.draw_piece()
+    # pieces.g_wp.draw_piece()
+    # pieces.h_wp.draw_piece()
 
-    pieces.a_bp.draw_piece()
-    pieces.b_bp.draw_piece()
-    pieces.c_bp.draw_piece()
-    pieces.d_bp.draw_piece()
-    pieces.e_bp.draw_piece()
-    pieces.f_bp.draw_piece()
-    pieces.g_bp.draw_piece()
-    pieces.h_bp.draw_piece()
+    # pieces.a_wr.draw_piece()
+    # pieces.b_wn.draw_piece()
+    # pieces.c_wb.draw_piece()
+    # pieces.wq.draw_piece()
+    # pieces.wk.draw_piece()
+    # pieces.f_wb.draw_piece()
+    # pieces.g_wn.draw_piece()
+    # pieces.h_wr.draw_piece()
 
-    pieces.a_br.draw_piece()
-    pieces.b_bn.draw_piece()
-    pieces.c_bb.draw_piece()
-    pieces.bq.draw_piece()
-    pieces.bk.draw_piece()
-    pieces.f_bb.draw_piece()
-    pieces.g_bn.draw_piece()
-    pieces.h_br.draw_piece()
+    # pieces.a_bp.draw_piece()
+    # pieces.b_bp.draw_piece()
+    # pieces.c_bp.draw_piece()
+    # pieces.d_bp.draw_piece()
+    # pieces.e_bp.draw_piece()
+    # pieces.f_bp.draw_piece()
+    # pieces.g_bp.draw_piece()
+    # pieces.h_bp.draw_piece()
+
+    # pieces.a_br.draw_piece()
+    # pieces.b_bn.draw_piece()
+    # pieces.c_bb.draw_piece()
+    # pieces.bq.draw_piece()
+    # pieces.bk.draw_piece()
+    # pieces.f_bb.draw_piece()
+    # pieces.g_bn.draw_piece()
+    # pieces.h_br.draw_piece()
 
 
 if __name__ == '__main__':
--- pieces.py
+++ pieces.py
@@ -4,9 +4,11 @@ import os
 from board import current_board
 
 
-class Piece:
+class Piece(p.sprite.Sprite):
 
     def __init__(self, board, display, turn, img, row, col, x, y):
+        p.sprite.Sprite.__init__(self)
+        self.img = img
         self.board = board
         self.display = display
         self.turn = turn
@@ -15,9 +17,22 @@ class Piece:
         self.col = col
         self.x = x
         self.y = y
+        self.image = p.Surface((95, 95))
+        self.image.blit()
+        # self.image = p.image.load(os.path.join("chess pngs",self.img))
+        self.rect = self.image.get_rect(topleft=[self.x, self.y])
 
     def draw_piece(self):
-        self.display.blit(p.image.load(os.path.join("chess pngs", self.img)), (self.x, self.y))
+        self.display.blit(self.image, self.rect)
+        # loaded_img = p.image.load(os.path.join("chess pngs", self.img))
+        # rect = p.draw.rect(main.screen, [0], [self.x, self.y, 95, 95])
+        # rect = loaded_img.get_rect(topleft=[self.x, self.y])
+        # piece_group.draw(main.screen)
+    def update(self):
+        self.rect.right + 5
+
+    def if_selected(self, rect):
+        pass
 
     def move(self):
         if self.turn % 2 == 0:
@@ -73,39 +88,42 @@ class Queen(Piece):
 
 
 #instances of the piece classes
+all_sprites = p.sprite.Group()
 
 a_wp = Pawn(current_board, main.screen, 0, 'wp.png', 2, 1, 45, 600)
-b_wp = Pawn(current_board, main.screen, 0, 'wp.png', 2, 2, 140, 600)
-c_wp = Pawn(current_board, main.screen, 0, 'wp.png', 2, 3, 235, 600)
-d_wp = Pawn(current_board, main.screen, 0, 'wp.png', 2, 4, 330, 600)
-e_wp = Pawn(current_board, main.screen, 0, 'wp.png', 2, 5, 425, 600)
-f_wp = Pawn(current_board, main.screen, 0, 'wp.png', 2, 6, 520, 600)
-g_wp = Pawn(current_board, main.screen, 0, 'wp.png', 2, 7, 615, 600)
-h_wp = Pawn(current_board, main.screen, 0, 'wp.png', 2, 8, 710, 600)
 
-a_wr = Rook(current_board, main.screen, 0, 'wr.png', 1, 1, 45, 695)
-b_wn = Knight(current_board, main.screen, 0, 'wn.png', 1, 2, 140, 695)
-c_wb = Bishop(current_board, main.screen, 0, 'wb.png', 1, 3, 235, 695)
-wq = Queen(current_board, main.screen, 0, 'wq.png', 1, 4, 330, 695)
-wk = King(current_board, main.screen, 0, 'wk.png', 1, 5, 425, 695)
-f_wb = Bishop(current_board, main.screen, 0, 'wb.png', 1, 6, 520, 695)
-g_wn = Knight(current_board, main.screen, 0, 'wn.png', 1, 7, 615, 695)
-h_wr = Rook(current_board, main.screen, 0, 'wr.png', 1, 8, 710, 695)
+all_sprites.add(a_wp)
+# b_wp = Pawn(current_board, main.screen, 0, 'wp.png', 2, 2, 140, 600)
+# c_wp = Pawn(current_board, main.screen, 0, 'wp.png', 2, 3, 235, 600)
+# d_wp = Pawn(current_board, main.screen, 0, 'wp.png', 2, 4, 330, 600)
+# e_wp = Pawn(current_board, main.screen, 0, 'wp.png', 2, 5, 425, 600)
+# f_wp = Pawn(current_board, main.screen, 0, 'wp.png', 2, 6, 520, 600)
+# g_wp = Pawn(current_board, main.screen, 0, 'wp.png', 2, 7, 615, 600)
+# h_wp = Pawn(current_board, main.screen, 0, 'wp.png', 2, 8, 710, 600)
 
-a_bp = Pawn(current_board, main.screen, 0, 'bp.png', 7, 1, 45, 125)
-b_bp = Pawn(current_board, main.screen, 0, 'bp.png', 7, 2, 140, 125)
-c_bp = Pawn(current_board, main.screen, 0, 'bp.png', 7, 3, 235, 125)
-d_bp = Pawn(current_board, main.screen, 0, 'bp.png', 7, 4, 330, 125)
-e_bp = Pawn(current_board, main.screen, 0, 'bp.png', 7, 5, 425, 125)
-f_bp = Pawn(current_board, main.screen, 0, 'bp.png', 7, 6, 520, 125)
-g_bp = Pawn(current_board, main.screen, 0, 'bp.png', 7, 7, 615, 125)
-h_bp = Pawn(current_board, main.screen, 0, 'bp.png', 7, 8, 710, 125)
+# a_wr = Rook(current_board, main.screen, 0, 'wr.png', 1, 1, 45, 695)
+# b_wn = Knight(current_board, main.screen, 0, 'wn.png', 1, 2, 140, 695)
+# c_wb = Bishop(current_board, main.screen, 0, 'wb.png', 1, 3, 235, 695)
+# wq = Queen(current_board, main.screen, 0, 'wq.png', 1, 4, 330, 695)
+# wk = King(current_board, main.screen, 0, 'wk.png', 1, 5, 425, 695)
+# f_wb = Bishop(current_board, main.screen, 0, 'wb.png', 1, 6, 520, 695)
+# g_wn = Knight(current_board, main.screen, 0, 'wn.png', 1, 7, 615, 695)
+# h_wr = Rook(current_board, main.screen, 0, 'wr.png', 1, 8, 710, 695)
 
-a_br = Rook(current_board, main.screen, 0, 'br.png', 8, 1, 45, 30)
-b_bn = Knight(current_board, main.screen, 0, 'bn.png', 8, 2, 140, 30)
-c_bb = Bishop(current_board, main.screen, 0, 'bb.png', 8, 3, 235, 30)
-bq = Queen(current_board, main.screen, 0, 'bq.png', 8, 4, 330, 30)
-bk = King(current_board, main.screen, 0, 'bk.png', 8, 5, 425, 30)
-f_bb = Bishop(current_board, main.screen, 0, 'bb.png', 8, 6, 520, 30)
-g_bn = Knight(current_board, main.screen, 0, 'bn.png', 8, 7, 615, 30)
-h_br = Rook(current_board, main.screen, 0, 'br.png', 8, 8, 710, 30)
+# a_bp = Pawn(current_board, main.screen, 0, 'bp.png', 7, 1, 45, 125)
+# b_bp = Pawn(current_board, main.screen, 0, 'bp.png', 7, 2, 140, 125)
+# c_bp = Pawn(current_board, main.screen, 0, 'bp.png', 7, 3, 235, 125)
+# d_bp = Pawn(current_board, main.screen, 0, 'bp.png', 7, 4, 330, 125)
+# e_bp = Pawn(current_board, main.screen, 0, 'bp.png', 7, 5, 425, 125)
+# f_bp = Pawn(current_board, main.screen, 0, 'bp.png', 7, 6, 520, 125)
+# g_bp = Pawn(current_board, main.screen, 0, 'bp.png', 7, 7, 615, 125)
+# h_bp = Pawn(current_board, main.screen, 0, 'bp.png', 7, 8, 710, 125)
+
+# a_br = Rook(current_board, main.screen, 0, 'br.png', 8, 1, 45, 30)
+# b_bn = Knight(current_board, main.screen, 0, 'bn.png', 8, 2, 140, 30)
+# c_bb = Bishop(current_board, main.screen, 0, 'bb.png', 8, 3, 235, 30)
+# bq = Queen(current_board, main.screen, 0, 'bq.png', 8, 4, 330, 30)
+# bk = King(current_board, main.screen, 0, 'bk.png', 8, 5, 425, 30)
+# f_bb = Bishop(current_board, main.screen, 0, 'bb.png', 8, 6, 520, 30)
+# g_bn = Knight(current_board, main.screen, 0, 'bn.png', 8, 7, 615, 30)
+# h_br = Rook(current_board, main.screen, 0, 'br.png', 8, 8, 710, 30)
